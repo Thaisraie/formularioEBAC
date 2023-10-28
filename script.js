f2f66a25a1a8636c05823bb5cc1d475a7fc3334e
Thaisraie <thaisdivino21@gmail.com>
2023-10-27 21:07:04 -0300

    correçao de tarefa

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function validaCampo() {
     
-    const valorA = document.getElementById('a').value
-    const valorB = document.getElementById('b').value
-
+    const valorA = parseInt(document.getElementById('a').value)
+    const valorB = parseInt(document.getElementById('b').value)
+    
     if(valorB > valorA) {
         document.getElementById('res').innerHTML = 'Válido' 
         document.getElementById('v').style.backgroundColor = '#a3d298'
     }
-    else {
+    if(valorB < valorA) {
         document.getElementById('res').innerHTML = `Inválido` 
         document.getElementById('v').style.backgroundColor = '#e09f9f' 
     }
+    if(!valorB || !valorA) {
+        document.getElementById('res').innerHTML = 'Insira um número'
+        document.getElementById('v').style.backgroundColor = 'white'
+        document.getElementById('v').style.width = '32%'
+    }   
 }
 
